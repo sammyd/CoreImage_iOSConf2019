42e6b4bbe675c7868cae2e0edb882b35ea54b0e1
Sam Davies <sam@razeware.com>
2019-04-12 10:18:32 +0200

    Phase 1: Demonstrating image reading, showing and filtering

--- CoreImage.playground/Contents.swift
+++ CoreImage.playground/Contents.swift
@@ -2,12 +2,23 @@ import UIKit
 import PlaygroundSupport
 
 //: # Loading Images
-
+let url = Bundle.main.url(forResource: "IMG_5276", withExtension: "HEIC")!
+var options = [CIImageOption.applyOrientationProperty: true]
+let image = CIImage(contentsOf: url, options: options)!
 
 //: # Filtering Images
+let resized = image.transformed(by: CGAffineTransform(scaleX: 0.125, y: 0.125))
 
+let filtered = resized.applyingFilter("CIPhotoEffectNoir")
+                      .applyingFilter("CIDotScreen", parameters: [
+                        "inputAngle" : 0.5,
+                        "inputCenter": CIVector(x: 100, y: 100),
+                        "inputSharpness" : 0.2,
+                        "inputWidth" : 0.5])
 
 //: # Displaying Images
+let uiImage = UIImage(ciImage: filtered)
+PlaygroundPage.current.liveView = UIImageView(image: uiImage)
 
 
 
